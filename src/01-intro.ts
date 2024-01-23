12c6b1041b9b607bf8ba0ca29a7759b5e2588207
VipulGajbhiye09 <vipulgajbhiye099@gmail.com>
2024-01-23 22:34:42 +0530

    Initial Commit

---
+++ src/01-intro.ts
@@ -0,0 +1,22 @@
+// TypeScript is a strongly typed programming language that builds on JavaScript,
+// giving you better tooling at any scale.
+
+//Typescript is all about Type Safety , Typescript does Static Checking
+
+// TypeScript adds additional syntax to JavaScript to support a tighter protocols to JS
+// to catch errors early in your editor & prevent runtime errors
+
+//Types in TypeScript : number, boolean, string, null, undefined, void, Object, Array, Tuples
+//Few Special types in TS : any, never, unknown
+
+//string
+let greeting: string = "Hello I'm Vipul";
+console.log(greeting);
+
+//number
+let myNum: number = 81.5;
+console.log(myNum);
+//boolean
+
+let isLoggedIn: boolean = true;
+console.log(isLoggedIn);
