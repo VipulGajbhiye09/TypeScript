ef4e416338f63fd00edcdf8adcf8e6dd4b51a2d5
VipulGajbhiye09 <vipulgajbhiye099@gmail.com>
2024-01-24 17:47:33 +0530

    Add objects.ts

---
+++ src/04-objects.ts
@@ -0,0 +1,26 @@
+// Objects
+
+const vipulObj = {
+    name: "vipul",
+    email: "v@v.com",
+    isActive: true
+}
+// Syntax for passing obj to function
+function createUser({name: string, isPaid: boolean}){
+    console.log("created user");
+}
+
+// ODD BEHAVIOR OF OBJECTS
+
+// Below line isn't allowed as it tries to pass 3 arguments but func accepts only 2 args
+// createUser({ name: "vipul", isPaid: false, email: "v@v.com" });
+
+// Odd part is by declaring it as a variable then passing that var to func is accepted as shown below
+let newUser = { name: "vipul", isPaid: false, email: "v@v.com" };
+createUser(newUser)
+
+// Return obj from function
+function createCourse():{name: string, price: number}{
+    return {name: "react.js", price: 399}
+}
+
