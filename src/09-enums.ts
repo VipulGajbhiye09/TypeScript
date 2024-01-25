b4d4f5e61aed7155c39b60bc71b9fc8e84c3e59e
VipulGajbhiye09 <vipulgajbhiye099@gmail.com>
2024-01-25 23:43:03 +0530

    Add enums.ts

---
+++ src/09-enums.ts
@@ -0,0 +1,12 @@
+// An enum is a special "class" that represents a group of constants (unchangeable variables).
+
+// Enums come in two flavors string and numeric. Lets start with numeric.
+
+enum SeatChoice {
+  AISLE,
+  MIDDLE,
+  WINDOW,
+  FOURTH,
+}
+
+const mySeat = SeatChoice.AISLE;
