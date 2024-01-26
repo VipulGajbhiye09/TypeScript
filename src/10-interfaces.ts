b9231edb06863d33b2011772f8fda9590b584d87
VipulGajbhiye09 <vipulgajbhiye099@gmail.com>
2024-01-26 21:03:24 +0530

    Update interfaces.ts

--- src/10-interfaces.ts
+++ src/10-interfaces.ts
@@ -1,5 +1,8 @@
 // Interfaces are similar to type aliases, except they only apply to object types.
 
+//Type aliases and interfaces are very similar, and in many cases you can choose between them freely.
+// Almost all features of an interface are available in type, the key distinction is that a type cannot be re-opened to add new properties vs an interface which is always extendable.
+
 interface User2 {
     email: string,
     userId: number,
@@ -9,9 +12,21 @@ interface User2 {
     func_name_2(username : string, value: number): number
 }
 
-const some_user: User2 = {
+//Re opening interface user2
+interface User2 {
+    githubToken: string
+}
+
+// extending admin2 to use user2's properties
+interface Admin2 extends User2 {
+  role: "sr-admin" | "jr-admin";
+}
+
+const some_user: Admin2 = {
   email: "v@v.com",
   userId: 55,
+  role: "sr-admin",
+  githubToken: "github",
   func_name: () => {
     return "trail started";
   },
